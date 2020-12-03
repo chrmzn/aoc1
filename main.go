bc2839c742dcb147b1eb8be52755ee865b21704e
Christopher O'Brien <chris@theobriens.co.nz>
2020-12-04 09:48:32 +1300

    working example

--- main.go
+++ main.go
@@ -12,20 +12,26 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("File contents: %s", contents)
+	// fmt.Printf("File contents: %s", contents)
 
 	data := string(contents)
 	rowData := strings.Split(data, "\n")
 
-	// var previousData []int
+	var previousData []int64
 
 	for _, line := range rowData {
-		fmt.Println(line)
 		i, err := strconv.ParseInt(line, 10, 0)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("Line: %d\n", i)
+		// fmt.Printf("Line: %d\n", i)
+		for _, val := range previousData {
+			if val+i == 2020 {
+				prod := val * i
+				fmt.Printf("Equal to 2020: %d, %d; Product: %d\n", val, i, prod)
+			}
+		}
+		previousData = append(previousData, i)
 
 	}
 }
