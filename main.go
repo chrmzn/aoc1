f9bf0f81b07729a59f0251401fc9503aca3e135a
Christopher O'Brien <chris@theobriens.co.nz>
2020-12-04 09:34:27 +1300

    looping over the contents

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 func main() {
-	data, err := ioutil.ReadFile("data.txt")
+	contents, err := ioutil.ReadFile("data.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("File contents: %s", data)
+	fmt.Printf("File contents: %s", contents)
 
+	data := string(contents)
+	rowData := strings.Split(data, "\n")
+
+	var previousData []int
+
+	for _, line := range rowData {
+		fmt.Println(line)
+
+	}
 }
